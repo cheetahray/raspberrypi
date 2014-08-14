3e5a784a4d923842969e24731d53695b1712df38
cheetahray <ray@dac.tw>
2014-08-14 14:02:05 +0800

    input

--- led_on.py
+++ led_on.py
@@ -4,20 +4,12 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-LED_PIN = 12
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-Try:
+BTN_PIN = 11
+GPIO.setup(BTN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+try :
     while True:
-        print("LED is on")
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(1)
-        print("LED is off")
-        GPIO.output(LED_PIN, GPIO.LOW)
-        time.sleep(1)
-except KeyboardInterrupt :
-    print "KeyboardInterrupt exception"
-except :
-    print "Other error or exception occurred!"
-finally :
+        if GPIO.input(BTN_PIN) == GPIO.LOW :
+            print("Button.Click")
+
+except KeyboardInterrupt:
     GPIO.cleanup()
