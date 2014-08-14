b8858216d0e937bde2f1cf50ed20d7fcddee7f67
cheetahray <ray@dac.tw>
2014-08-14 13:41:45 +0800

    Try 有問題?

--- led_on.py
+++ led_on.py
@@ -7,8 +7,17 @@ GPIO.setmode(GPIO.BOARD)
 LED_PIN = 12
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-print("LED is on")
-GPIO.output(LED_PIN, GPIO.HIGH)
-time.sleep(3)
-
-GPIO.cleanup()
+Try:
+    while True:
+        print("LED is on")
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(1)
+        print("LED is off")
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(1)
+except KeyboardInterrupt :
+    print "KeyboardInterrupt exception"
+except :
+    print "Other error or exception occurred!"
+finally :
+    GPIO.cleanup()
