c543e907a2e96a9f3c7590537054f366e9639bca
cheetahray <ray@dac.tw>
2015-03-31 10:54:42 +0800

    把 thread stop 移到 finally

--- rayxbmc.py
+++ rayxbmc.py
@@ -49,12 +49,12 @@ try:
         print >>sys.stderr, 'received "%s"' % data
         if data == recemsg:
             counter += 1
-    tt.stop()
-
+    
 finally:
     print >>sys.stderr, 'closing socket'
     sock.close()
-    
+		tt.stop()
+  
  
 #Base URL of the json RPC calls. For GET calls we append a "request" URI 
 #parameter. For POSTs, we add the payload as JSON the the HTTP request body
