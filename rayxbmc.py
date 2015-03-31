e276908da8e1ec1a6f2da3f786a604c67cfe050c
cheetahray <ray@dac.tw>
2015-03-31 10:33:42 +0800

    把 url get 先取消掉

--- rayxbmc.py
+++ rayxbmc.py
@@ -1,6 +1,16 @@
 import requests
 import json
 import urllib
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('192.168.11.255', 52790)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+#sock.bind(server_address)
  
 #Required header for XBMC JSON-RPC calls, otherwise you'll get a 
 #415 HTTP response code - Unsupported media type
@@ -13,41 +23,39 @@ xbmc_host = 'localhost'
  
 #Configured in Settings -> Services -> Webserver -> Port
 xbmc_port = 8080
+
+counter = 1
+
+sendmsg = '1'
+recemsg = '2'
+try:
+    
+    while 2 > counter:
+        # Send data
+        print >>sys.stderr, 'sending "%s"' % sendmsg
+        sent = sock.sendto(sendmsg, server_address)
+	      # Receive response
+        print >>sys.stderr, 'waiting to receive'
+        data = sock.recvfrom(4096)
+        print >>sys.stderr, 'received "%s"' % data
+	      if data == recemsg
+	           counter += 1
+        time.sleep(1)
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
+    
  
 #Base URL of the json RPC calls. For GET calls we append a "request" URI 
 #parameter. For POSTs, we add the payload as JSON the the HTTP request body
 xbmc_json_rpc_url = "http://" + xbmc_host + ":" + str(xbmc_port) + "/jsonrpc"
  
-#Payload for the method to get the currently playing / paused video or audio
-payload = {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}
-url_param = urllib.urlencode({'request': json.dumps(payload)})
- 
-response = requests.get(xbmc_json_rpc_url + '?' + url_param, 
-                        headers=headers)
- 
-#response.text will look like this if something is playing
-#{"id":1,"jsonrpc":"2.0","result":[{"playerid":1,"type":"video"}]}
-#and if nothing is playing:
-#{"id":1,"jsonrpc":"2.0","result":[]}
-print response.text     
-data = json.loads(response.text)
-#result is an empty list if nothing is playing or paused. 
-if data['result']:
-    #We need the specific "playerid" of the currently playing file in order 
-    #to pause it
-    player_id = data['result'][0]["playerid"]
- 
-    payload = {"jsonrpc": "2.0", "method": "Player.PlayPause", 
-               "params": { "playerid": player_id }, "id": 1}
-    response = requests.post(xbmc_json_rpc_url, data=json.dumps(payload), 
-                             headers=headers)
-     
-    #response.text will look like this if we're successful:
-    #{"id":1,"jsonrpc":"2.0","result":{"speed":0}}
-else:
-    payload = {"jsonrpc": "2.0", "method": "Player.Open", 
+payload = {"jsonrpc": "2.0", "method": "Player.Open", 
                "params": {"item":{"playlistid":1, "position" : 0}}, "id": 1}
-    #payload = {"jsonrpc": "2.0", "method": "Player.Open", 
-    #           "params": { "item": { "file": "/home/pi/Palmipedarium_AVC_HD.mp4" }}, "id": 1}
-    response = requests.post(xbmc_json_rpc_url, data=json.dumps(payload), 
+#payload = {"jsonrpc": "2.0", "method": "Player.Open", 
+#           "params": { "item": { "file": "/home/pi/Palmipedarium_AVC_HD.mp4" }}, "id": 1}
+response = requests.post(xbmc_json_rpc_url, data=json.dumps(payload), 
                              headers=headers)
+
+print response.text     
