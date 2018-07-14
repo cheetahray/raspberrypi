0bfadb099c89b5ac2fdc0066aa99924ff638d345
cheetahray <ray608@gmail.com>
2018-07-14 18:15:54 +0800

    change listen to 3

--- Bicycle/MiLan.py
+++ Bicycle/MiLan.py
@@ -109,7 +109,7 @@ bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
-server.listen(2)  # max backlog of connections
+server.listen(3)  # max backlog of connections
 
 print 'Listening on {}:{}'.format(bind_ip, bind_port)
 
